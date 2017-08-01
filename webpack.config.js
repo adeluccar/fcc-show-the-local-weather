72f8107bde71b99f378d7164652f425796dbad0a
Alberto De Lucca <albertodelucca@gmail.com>
2017-07-31 21:59:58 -0400

    Install webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,9 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+  },
   module: {
     rules: [
       {
