0305cde4110562471f7c6088f24795d4164557bb
Alberto De Lucca <albertodelucca@gmail.com>
2017-08-01 17:55:21 -0400

    Add geolocation
    Resources
    
    - https://developer.mozilla.org/en-US/docs/Web/API/Geolocation/Using_geolocation
    - https://developers.google.com/web/updates/2016/04/geolocation-on-secure-contexts-only
    - https://www.sitepoint.com/using-the-html5-geolocation-api/
    - https://www.sitepoint.com/html5-geolocation/

--- src/app.js
+++ src/app.js
@@ -3,11 +3,6 @@ import style from './app.css';
 
 require.context('./img');
 
-const proxy = 'https://fcc-climate-proxy.herokuapp.com';
-const lat = '10.500000';
-const lon = '-66.916664';
-const url = `${proxy}/?lat=${lat}&lon=${lon}`;
-
 function getMyElement(className) {
   const arry = document.getElementsByClassName(className);
   const element = arry.item(0);
@@ -61,13 +56,29 @@ function convertUnits() {
   }
 }
 
+function okPosition(position) {
+  const proxy = 'https://fcc-climate-proxy.herokuapp.com';
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `${proxy}/?lat=${lat}&lon=${lon}`;
+
+  fetch(url)
+    .then(resp => resp.json())
+    .then(data => displayWeather(parseForecast(data)));
+}
+
+function errorPosition() {
+  alert('weird stuff happened');
+}
+
+if ('geolocation' in navigator) {
+  navigator.geolocation.getCurrentPosition(okPosition, errorPosition);
+} else {
+  alert('Geolocation is not supported on this browser');
+}
+
 document.querySelector('#button').addEventListener('click', convertUnits);
 
-// fetch(url)
-//   .then(resp => resp.json())
-//   .then(data => displayWeather(parseForecast(data)))
-//   .catch(error => console.log(error))
+// const mockdata = '{"latitude":10.480594,"longitude":-66.903606,"timezone":"America/Caracas","offset":-4,"currently":{"time":1501607253,"summary":"Drizzle","icon":"rain","precipIntensity":0.033,"precipProbability":0.34,"precipType":"rain","temperature":77.15,"apparentTemperature":78.1,"dewPoint":68.71,"humidity":0.75,"windSpeed":4.16,"windGust":6.29,"windBearing":52,"cloudCover":0.47,"pressure":1014.16,"ozone":274.32,"uvIndex":11}}';
 
-const mockdata = '{"latitude":10.480594,"longitude":-66.903606,"timezone":"America/Caracas","offset":-4,"currently":{"time":1501607253,"summary":"Drizzle","icon":"rain","precipIntensity":0.033,"precipProbability":0.34,"precipType":"rain","temperature":77.15,"apparentTemperature":78.1,"dewPoint":68.71,"humidity":0.75,"windSpeed":4.16,"windGust":6.29,"windBearing":52,"cloudCover":0.47,"pressure":1014.16,"ozone":274.32,"uvIndex":11}}';
-
-displayWeather(parseForecast(JSON.parse(mockdata)));
+// displayWeather(parseForecast(JSON.parse(mockdata)));
