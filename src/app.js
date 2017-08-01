b0086820b81656eb743d0ab4b6be3fe06baca209
Alberto De Lucca <albertodelucca@gmail.com>
2017-08-01 02:09:38 -0400

    Add first version of app
    Notes:
    Uses fetch API to execute a request to a proxy server running on heroku.
    For now, the location is hard coded (in order to debug the basic app
    functionality). I decided to try my hand at vanilla js instead of
    relying on jQuery. I also used ES6 syntax wherever possible.
    
    Resources
    
    - https://scotch.io/tutorials/how-to-use-the-javascript-fetch-api-to-get-data
    - https://davidwalsh.name/fetch
    - https://developer.mozilla.org/en-US/docs/Web/API/HTMLCollection/item
    - https://developer.mozilla.org/en/docs/Web/API/Document/getElementsByClassName
    - https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
    - https://gist.github.com/WarFox/3775927
    - http://www.sitepoint.com/forums/showthread.php?523221-Is-it-possible-to-change-innerHTML-of-dynamically-created-DIV
    - https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Math/round

--- src/app.js
+++ src/app.js
@@ -2,3 +2,35 @@ import 'normalize.css';
 import style from './app.css';
 
 require.context('./img');
+
+const proxy = 'https://fcc-climate-proxy.herokuapp.com';
+const lat = '10.500000';
+const lon = '-66.916664';
+const url = `${proxy}/?lat=${lat}&lon=${lon}`;
+
+function getMyElement(className) {
+  const arry = document.getElementsByClassName(className);
+  const element = arry.item(0);
+  return element;
+}
+
+function setWeatherData(data) {
+  const now = data.currently;
+
+  const appLocation = getMyElement('app__location');
+  appLocation.innerHTML = data.timezone;
+
+  const appTemperature = getMyElement('app__temperature');
+  appTemperature.innerHTML = `${Math.round(now.temperature)}º`;
+
+  const appIcon = getMyElement('app__icon');
+  appIcon.src = `img/${now.icon}.svg`;
+
+  const appSummary = getMyElement('app__summary');
+  appSummary.innerHTML = now.summary;
+}
+
+fetch(url)
+  .then(resp => resp.json())
+  .then(data => setWeatherData(data))
+  .catch(error => console.log(error));
